e96d5aecec6011ac21e4c3f3f4b6f744c71f866b
aabeas <aaron.beasley@gmail.com>
2018-03-05 17:44:00 -0600

    Created css image cards with placeholders for documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -10,28 +10,28 @@ import { Document } from './document'
 
 export class DocumentsComponent {
   pageTitle: string = "Document Dashboard"
-  
+
   documents: Document[] = [
     {
       title: "My First Document",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       file_url: "http://google.com",
       updated_at: "11/11/16",
-      image_url: "http://gogle.com",
+      image_url: "https://placeholdit.co//i/318x180?bg=0d48c4",
     },
     {
       title: "My Second Document",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       file_url: "http://google.com",
       updated_at: "12/12/17",
-      image_url: "http://gogle.com",
+      image_url: "https://placeholdit.co//i/318x180?bg=5178cc",
     },
     {
       title: "My Third Document",
       description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       file_url: "http://google.com",
       updated_at: "01/13/18",
-      image_url: "http://gogle.com",
+      image_url: "https://placeholdit.co//i/318x180?bg=849bcc",
     }
   ]
 }
